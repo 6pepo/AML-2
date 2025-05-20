Feature Normalization and Neuron fixes

Dataset/iono.py
@@ -14,6 +14,8 @@ import RandomForest.RF_Library as RF
 import torch
 
 from matplotlib.widgets import Slider
+from sklearn.preprocessing import StandardScaler
+from scipy.linalg import eigh
   
 start_cpu = clock.process_time()
 
@@ -118,8 +120,10 @@ for i, lab in enumerate(labels):
             bad_e += 1
 
 print(f'Good signal: {good_t+good_e}, Bad signal: {bad_t+bad_e}')
+
 corr = np.corrcoef(patt_train, rowvar=False)    
-e_val, e_vec = RF.torch_eig(corr, var_type=torch.float32)
+# e_val, e_vec = RF.torch_eig(corr, var_type=torch.float32)
+e_val, e_vec = eigh(corr)
 e_val = np.real(e_val)  #we cast to real because they have null imaginary part
 e_vec = np.real(e_vec)
 
@@ -134,19 +138,19 @@ perc_tresh = 0.9            # Threshold of features at wich we cut
 principal_components = np.zeros(len(e_val))
 cum_percentage = np.zeros(len(e_val))
 print('\nPercent\tCumulative')
-for i,val in enumerate(e_val):
+for i, val in enumerate(e_val):
     val_sum += val
     print(round(val/e_val_sum * 100, 2), '%\t', np.round(val_sum/e_val_sum * 100, 2), '%')
     principal_components[i] = i+1
     cum_percentage[i] = np.round(val_sum/e_val_sum * 100, 2)
-    if val_sum/e_val_sum > perc_tresh:
+    if val_sum/e_val_sum > perc_tresh and n_vec==0:
         n_vec = i+1
-        break
+        print('Threshold reached!')
 
 fig1, ax1 = plt.subplots()
 ax1.plot(principal_components,cum_percentage,'bo--' )
-ax1.hlines(cum_percentage[9], 0,16, colors='red')
-ax1.plot(principal_components[9], cum_percentage[9], 'ro')
+ax1.hlines(cum_percentage[n_vec-1], 0,16, colors='red')
+ax1.plot(principal_components[n_vec-1], cum_percentage[n_vec-1], 'ro')
 ax1.set_xlabel('N° Principal Compones')
 ax1.set_ylabel('Cumulative Percentage')
 ax1.grid(True)

NeuralNetwork/NN_Library.py
@@ -1034,7 +1034,7 @@ def NN_parameters_scanner(k, par_all, par_range, patterns, labels, label0, label
     return 0
 
 def confMat_binary_plot(conf_mat, accuracy=None, sensitivity=None, specificity=None, precision=None, title=None):
-    fig, ax = plt.subplots(figsize=(16,9))
+    fig, ax = plt.subplots()
     if title == None:
         ax.set_title("Confusion matrix")
     else:
@@ -1119,9 +1119,6 @@ def confMat_binary_plot(conf_mat, accuracy=None, sensitivity=None, specificity=N
 
 def gaussian(x,a,mean,sigma):
     return a*np.exp(-((x-mean)**2/(sigma**2))/2)
-
-def sigmoid(x):
-    return 1/(1+np.exp(-x))
 
 def hp_mode(mu1,mu2):
     if mu1 < mu2:
@@ -1216,7 +1213,7 @@ def squared_error(pred, target):
     return (pred - target)**2
 
 def step_squared_error(pred, target):
-    return 2*(pred - target)  * pred*(1-pred)
+    return (pred - target) * pred*(1-pred)
 
 def perceptron_loss(pred, target):
     return abs(pred - target)
@@ -1229,7 +1226,7 @@ def heavside(x):
 
 class neuron:
 
-    def __init__(self, labneg, labpos, max_epochs, learning_rate, tol = 1e-6, loss = 'squaredError', random_state = None):
+    def __init__(self, labneg, labpos, max_epochs, learning_rate, tol = 1e-3, loss = 'squaredError', random_state = None):
         self.labneg = labneg
         self.labpos = labpos
         self.epochs = max_epochs
@@ -1258,15 +1255,14 @@ class neuron:
         self.train_lab = np.where(target == self.labpos, 1., 0.)
         self.weights = np.empty(self.n_feat)
         for i in range(self.n_feat):
-            self.weights[i] = (random.random() - 0.5)* self.lr
-        self.bias = (random.random() - 0.5)* self.lr
+            self.weights[i] = 2*random.random() - 1
         
         self.loss_list = []
         self.conv_counter = 0
 
         for i in range(self.epochs):
             # Forward Propagation
-            self.weighted_sum = self.weights.dot(self.train_patt.transpose() + self.bias)
+            self.weighted_sum = self.weights.dot(self.train_patt.transpose())
             self.temp_pred = self.activ(self.weighted_sum)
             self.epoch_loss = 0.
 
@@ -1279,7 +1275,6 @@ class neuron:
                 self.step = self.d_cost(tpred, self.train_lab[k])
                 for j in range(self.n_feat):
                     self.weights[j] -= self.lr * self.step * self.train_patt[k, j]
-                self.bias -= self.lr * self.step
 
             # Convergence Check
             if self.conv_counter < 5 and i>0:
@@ -1294,5 +1289,5 @@ class neuron:
             self.loss_list.append(self.epoch_loss)
 
     def predict(self, patterns):
-        return np.where(self.activ(self.weights.dot(patterns.transpose()) + self.bias) > 0.5, self.labpos, self.labneg) 
+        return np.where(self.activ(self.weights.dot(patterns.transpose())) > 0.5, self.labpos, self.labneg) 
 

NeuralNetwork/NN_PCA_scanner.py
@@ -7,6 +7,7 @@ import os
 
 from matplotlib import cm, colors
 from matplotlib.widgets import Slider
+from sklearn.preprocessing import StandardScaler
 
 
 if __name__ == '__main__':
@@ -27,6 +28,10 @@ if __name__ == '__main__':
     labels = np.ravel(labels.to_numpy())
     label0 = 'b'
     label1 = 'g'
+
+    # Normalizing features
+    scaler = StandardScaler()
+    patterns = scaler.fit_transform(patterns)
 
     good = 0
     bad = 0
@@ -65,16 +70,27 @@ if __name__ == '__main__':
     normalization = colors.Normalize(vmin=np.min(scores), vmax=np.max(scores))
     loss_norm = colors.Normalize(vmin=np.min(loss), vmax=np.max(loss))
 
-    for i,k in enumerate(k_range):
-        fig, ax = plt.subplots(1,2, figsize=(16,9))
+    # Plots k-lr heatmap at last epoch
+    fig, ax = plt.subplots((1,2), figsize=(16,9))
 
-        sens_colormesh = NN.heatmap_plotter(ax[0], lr_range, epoch_range, sens[:,i,:], '{:.2f}', "Sensitivity", 'Learning Rate', 'Number of epochs', normalization, cm.viridis)
-        spec_colormesh = NN.heatmap_plotter(ax[1], lr_range, epoch_range, spec[:,i,:], '{:.2f}', "Specificity", 'Learning Rate', 'Number of epochs', normalization, cm.viridis)
+    sens_colormesh = NN.heatmap_plotter(ax[0], k_range, lr_range, sens[-1,:,:], '{:.2f}', "Sensitivity", 'Number of Folds', 'Learning Rate', normalization, cm.viridis)
+    spec_colormesh = NN.heatmap_plotter(ax[0], k_range, lr_range, spec[-1,:,:], '{:.2f}', "Specificity", 'Number of Folds', 'Learning Rate', normalization, cm.viridis)
+
+    fig.suptitle(f' Epoch {epoch_range[-1]}')
+    fig.colorbar(spec_colormesh, orientation='vertical')
+    fig.savefig(script_directory+f'/NN Not PCA/score_heatmaps/heatmap_{epoch_range[-1]}_epoch.png', dpi=240)
+    plt.close(fig)
+
+    for i,k in enumerate(k_range):
+        # fig, ax = plt.subplots(1,2, figsize=(16,9))
+
+        # sens_colormesh = NN.heatmap_plotter(ax[0], lr_range, epoch_range, sens[:,i,:], '{:.2f}', "Sensitivity", 'Learning Rate', 'Number of epochs', normalization, cm.viridis)
+        # spec_colormesh = NN.heatmap_plotter(ax[1], lr_range, epoch_range, spec[:,i,:], '{:.2f}', "Specificity", 'Learning Rate', 'Number of epochs', normalization, cm.viridis)
         
-        fig.suptitle(f'{k}-Fold')
-        fig.colorbar(spec_colormesh,  orientation='vertical')
-        fig.savefig(script_directory+f'/NN PCA/score_heatmaps/heatmap_{k}_fold.png', dpi=240)
-        plt.close(fig)
+        # fig.suptitle(f'{k}-Fold')
+        # fig.colorbar(spec_colormesh,  orientation='vertical')
+        # fig.savefig(script_directory+f'/NN PCA/score_heatmaps/heatmap_{k}_fold.png', dpi=240)
+        # plt.close(fig)
                 
         fig_loss, ax_loss = plt.subplots( figsize=(16,9))
 

NeuralNetwork/NN_comparator.py
@@ -10,6 +10,7 @@ import os
 from matplotlib import cm, colors
 from tqdm import tqdm
 from scipy.optimize import curve_fit
+from sklearn.preprocessing import StandardScaler
 
 def h_line(x,c):
     return np.ones(len(x))*c
@@ -35,22 +36,28 @@ if __name__ == '__main__':
 	epoch_PCA = 93
 
 	# Training Set
-	train_patterns = pd.read_csv(dataset_path+"/iono_trainPatt.csv", header=0, index_col=0)
+	orig_train_patterns = pd.read_csv(dataset_path+"/iono_trainPatt.csv", header=0, index_col=0)
 	train_labels = pd.read_csv(dataset_path+"/iono_trainLab.csv", header=0, index_col=0)
 
-	train_patterns = train_patterns.to_numpy()
+	orig_train_patterns = orig_train_patterns.to_numpy()
 	train_labels = train_labels.to_numpy().ravel()
 	label0 = 'b'
 	label1 = 'g'
 
 	# External test Set
-	ext_patterns = pd.read_csv(dataset_path+"/iono_extPatt.csv", header=0, index_col=0)
+	orig_ext_patterns = pd.read_csv(dataset_path+"/iono_extPatt.csv", header=0, index_col=0)
 	ext_labels = pd.read_csv(dataset_path+"/iono_extLab.csv", header=0, index_col=0)
 
-	ext_patterns = ext_patterns.to_numpy()
-	ext_labels = ext_labels.to_numpy().ravel()
+	orig_ext_patterns = orig_ext_patterns.to_numpy()
+	ext_labels = ext_labels.to_numpy().ravel() 
 
-	#NOT PCA
+	# NOT PCA
+	# Normalizing features
+	scaler = StandardScaler()
+	scaler.fit(orig_train_patterns)
+	train_patterns = scaler.transform(orig_train_patterns)
+	ext_patterns = scaler.transform(orig_ext_patterns)
+
 	acc_list = []
 	sens_list = []
 	spec_list = []
@@ -194,8 +201,14 @@ if __name__ == '__main__':
 	#PCA 
 	pc_mat = pd.read_csv(dataset_path+"/eigenvectors.csv", sep=',', index_col=0)
 	pc_mat = pc_mat.to_numpy(dtype=np.float32)
-	train_patterns = train_patterns.dot(pc_mat)
-	ext_patterns = ext_patterns.dot(pc_mat)
+	train_patterns = orig_train_patterns.dot(pc_mat)
+	ext_patterns = orig_ext_patterns.dot(pc_mat)
+
+	# Normalizing Features
+	PCA_scaler = StandardScaler()
+	PCA_scaler.fit(train_patterns)
+	train_patterns = PCA_scaler.transform(train_labels)
+	ext_patterns = PCA_scaler.transform(ext_patterns)
 
 	acc_list = []                   #CV metrics
 	sens_list = []